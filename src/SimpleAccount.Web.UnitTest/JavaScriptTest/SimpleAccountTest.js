4814e266f3da6a1bb97995ce8467bb28aff802a0
Siva.Kandaraj <siva.kandaraj@myob.com>
2013-10-04 19:05:57 +1000

    Added missing karma files

---
+++ src/SimpleAccount.Web.UnitTest/JavaScriptTest/SimpleAccountTest.js
@@ -0,0 +1,52 @@
+﻿
+
+
+describe("Simple account JavaScript Tests ->", function () {
+
+    beforeEach(function () {
+        simpleAccountModule = module("simpleAccountApp");
+    });
+
+
+    describe('Simple Account Controller Tests ', function() {
+
+        var scope, ctrl, simpleService = {};
+        
+        beforeEach(inject(function ($rootScope, $controller, $q) {
+            scope = $rootScope.$new();             
+            q = $q;
+            var model = {};
+            model.email = "";
+            model.password = "";
+            model.confirmPassword = "";
+            model.errorMessage = "";
+            model.successMessage = "";
+            simpleService.model = model;
+            ctrl = $controller('simpleAccountController', { $scope: scope, simpleAccountService: simpleService });
+        }));
+        
+        it('should have model from service and be empty', function() {
+            expect(scope.model.email).toBe('');
+            expect(scope.model.password).toBe('');
+            expect(scope.model.confirmPassword).toBe('');
+            expect(scope.model.errorMessage).toBe('');
+            expect(scope.model.successMessage).toBe('');
+            expect(scope.strength).toBe('');
+            expect(scope.passwordNotMatched).toBe(false);
+            expect(scope.IsFormValid()).toBe(false);
+        });
+
+
+        it('should watch password matching', function() {
+            scope.model.password = 'test';
+            scope.model.confirmPassword = 'test1';
+            scope.$digest();
+            expect(scope.passwordNotMatched).toBe(true);
+             
+            scope.model.confirmPassword = 'test';
+            scope.$digest();
+            expect(scope.passwordNotMatched).toBe(false);
+        });
+    });
+    
+});
---
+++ src/SimpleAccount.Web.UnitTest/JavaScriptTest/karma.config.js
@@ -0,0 +1,96 @@
+﻿/// <reference path="../helper/angular-scenario.js" />
+/// <reference path="../helper/jquery.min.js" />
+
+module.exports = function (config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../..',
+
+        frameworks: ['jasmine'],
+
+        // list of files / patterns to load in the browser
+        files: [
+        'SimpleAccount.Web.UnitTest/Helper/jquery.min.js',
+        'SimpleAccount.Web/Js/angular.min.js',
+        'simpleaccount.web/js/angular-sanitize.min.js',
+        'SimpleAccount.Web.UnitTest/Helper/angular.mocks.js', 
+        'SimpleAccount.Web/Js/App.js',
+        'SimpleAccount.Web/Js/SimpleAccountService.js',
+        'SimpleAccount.Web/Js/SimpleAccountController.js',
+        'SimpleAccount.Web.UnitTest/JavaScriptTest/*Test.js'
+        ],
+
+        // list of files to exclude
+        exclude: [
+            'SimpleAccount.Web.UnitTest/JavaScriptTest/*.conf.js'
+        ],
+
+        
+        preprocessors: {
+            '**/SimpleAccount.Web/Js/**/!(angular|bootstrap|jquery).js': ['coverage']
+        },
+        
+        // use dots reporter, as travis terminal does not support escaping sequences
+        // possible values: 'dots', 'progress'
+        // CLI --reporters progress
+        reporters: ['progress', 'coverage', 'junit'],
+
+        junitReporter: {
+            // will be resolved to basePath (in the same way as files/exclude patterns)
+            outputFile: 'jstestresults.xml'
+        },
+
+        coverageReporter: {
+            type: 'cobertura',
+            dir: 'coverage/',
+            file: 'coverage.xml'
+        },
+        
+        // web server port
+        // CLI --port 9876
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        // CLI --colors --no-colors
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        // CLI --log-level debug
+        logLevel: config.LOG_INFO,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        // CLI --auto-watch --no-auto-watch
+        autoWatch: false,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        // CLI --browsers Chrome,Firefox,Safari
+        browsers: ['PhantomJS'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        // CLI --capture-timeout 5000
+        captureTimeout: 20000,
+
+        // Auto run tests on start (when browsers are captured) and exit
+        // CLI --single-run --no-single-run
+        singleRun: true,
+
+        // report which specs are slower than 500ms
+        // CLI --report-slower-than 500
+        reportSlowerThan: 500,
+
+        plugins: [
+          'karma-jasmine',
+          'karma-phantomjs-launcher',
+          'karma-junit-reporter',
+          'karma-commonjs', 'karma-coverage'
+        ]
+    });
+};
